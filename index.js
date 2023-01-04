4fad50b21282fc06eed4ce850d07c86c0d2d3c1d
salehsalehi1234 <121737649+salehsalehi1234@users.noreply.github.com>
2023-01-04 05:27:12 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
